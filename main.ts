1a42f498ad70e9a12d4ff124b6ff4fc8cb358bd7
mm0913 <64566949+mm0913@users.noreply.github.com>
2020-04-30 11:25:38 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
 //% weight=10 icon="\uf085" color=#FFA400 block="m-robo"
-namespace comment {
-    //% blockId=show_strings block="mi-03 %v"
-    export function noaction(text: string): void {
+namespace robo_test {
+    let a;
+
+    //% blockId=plus block="plus %v"
+    export function plus(){
+        a = 1+1;
+    }
+    
+    //% blockId=answer block="answer %v"
+    export function answer(){
+        return(123);
     }
 }
