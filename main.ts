5da01ab80a1aacf962c72cf9fef2dae0255af617
mm0913 <64566949+mm0913@users.noreply.github.com>
2020-04-30 10:56:11 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=150 icon="\uf075" color=#FFFF33 block="m-robo"
+//% weight=10 icon="\uf075" color=#FFFF99 block="m-robo"
 namespace comment {
     //% blockId=show_strings block="mi-03 %v"
     export function noaction(text: string): void {
